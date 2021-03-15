e1f740d25ab6c4a99112f222caad6f35ce0f3058
w3bh4ck <lucky@softcom.ng>
2021-03-15 23:52:11 +0100

    Added the signup page

---
+++ pages/signup/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { firebase } from '../../src/utils/firbase-config';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onRegister = async e => {
+    e.preventDefault();
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      if (error) {
+        return error;
+      }
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onRegister}>
+        <div>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
