38c6781b958c2a97fbbf0fda798bfcdce1b1888d
w3bh4ck <lucky@softcom.ng>
2021-03-16 00:07:10 +0100

    Started with Material UI themeing

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import { AuthProvider } from '../src/providers/auth-provider/Auth-provider';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
