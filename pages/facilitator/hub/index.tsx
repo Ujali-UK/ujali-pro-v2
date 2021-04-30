c9cbd931d66b3ecc98198d2ecf5366d221a92f24
w3bh4ck <lucky@softcom.ng>
2021-04-30 19:38:19 +0100

    Update: added the subnav

--- pages/facilitator/hub/index.tsx
+++ pages/facilitator/hub/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/layout';
 import React from 'react';
+import Subnav from '../../../src/components/navbar/Subnav';
 import Protected from '../../../src/layout/Protected';
 
 const Hub = () => {
   return (
     <Protected>
+      <Subnav />
       <Box>hub updated</Box>
     </Protected>
   );
--- src/components/navbar/Subnav.tsx
+++ src/components/navbar/Subnav.tsx
@@ -1,35 +1,96 @@
 import Icon from '@chakra-ui/icon';
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
 import { Box, Text } from '@chakra-ui/layout';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { MdSearch } from 'react-icons/md';
 
 const Subnav = () => {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState('chats');
+
   return (
     <Box height="5rem" bgColor="brand.gray" width="full">
-      <Box d="flex" justifyContent="space-between" px="1rem">
-        <Box pt="1rem" width={{ base: 'full', md: '50%' }}>
-          <InputGroup size="lg" color="white">
-            <InputLeftElement
-              pointerEvents="none"
-              color="white"
-              fontSize="1.2em"
-              children={<Icon as={MdSearch} h="8" w="8" />}
-            />
-            <Input
-              _placeholder={{ color: 'white' }}
-              bgColor="brand.orange"
-              color="white"
-              placeholder="Search gigs..."
-              borderRadius="lg"
-            />
-          </InputGroup>
+      {router.pathname.includes('gigs') && (
+        <Box d="flex" justifyContent="space-between" px="1rem">
+          <Box pt="1rem" width={{ base: 'full', md: '50%' }}>
+            <InputGroup size="lg" color="white">
+              <InputLeftElement
+                pointerEvents="none"
+                color="white"
+                fontSize="1.2em"
+                children={<Icon as={MdSearch} h="8" w="8" />}
+              />
+              <Input
+                _placeholder={{ color: 'white' }}
+                bgColor="brand.orange"
+                color="white"
+                placeholder="Search gigs..."
+                borderRadius="lg"
+              />
+            </InputGroup>
+          </Box>
+          <Box>
+            <Text color="white" pt="1.5rem">
+              Showing: <span style={{ fontWeight: 'bold' }}>1</span> out of 1{' '}
+              <span style={{ fontWeight: 'bold' }}>Gigs</span>
+            </Text>
+          </Box>
         </Box>
-        <Box>
-          <Text color="white" pt="1.5rem">
-            Showing: <span style={{ fontWeight: 'bold' }}>1</span> out of 1{' '}
-            <span style={{ fontWeight: 'bold' }}>Gigs</span>
-          </Text>
+      )}
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        px={{ base: '1rem', md: '8rem' }}
+        pt="1.5rem"
+      >
+        <Box
+          onClick={() => setActiveTab('chats')}
+          _hover={{ border: '1px solid white' }}
+          cursor="pointer"
+          color={'white'}
+          fontWeight="bold"
+          border={activeTab === 'chats' ? '1px solid orange' : ''}
+          px="2rem"
+          borderRadius="md"
+        >
+          <Text>CHATS</Text>
+        </Box>
+        <Box
+          onClick={() => setActiveTab('bookings')}
+          _hover={{ border: '1px solid white' }}
+          cursor="pointer"
+          color={'white'}
+          fontWeight="bold"
+          border={activeTab === 'bookings' ? '1px solid orange' : ''}
+          px="2rem"
+          borderRadius="md"
+        >
+          <Text>BOOKINGS</Text>
+        </Box>
+        <Box
+          onClick={() => setActiveTab('billing')}
+          _hover={{ border: '1px solid white' }}
+          cursor="pointer"
+          color={'white'}
+          fontWeight="bold"
+          border={activeTab === 'billing' ? '1px solid orange' : ''}
+          px="2rem"
+          borderRadius="md"
+        >
+          <Text>BILLING</Text>
+        </Box>
+        <Box
+          onClick={() => setActiveTab('calendar')}
+          _hover={{ border: '1px solid white' }}
+          cursor="pointer"
+          color={'white'}
+          fontWeight="bold"
+          border={activeTab === 'calendar' ? '1px solid orange' : ''}
+          px="2rem"
+          borderRadius="md"
+        >
+          <Text>CALENDAR</Text>
         </Box>
       </Box>
     </Box>
